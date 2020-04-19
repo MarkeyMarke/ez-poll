ab2c8d94437744d5d009dca50f17798d7ffc8271
Mark <casapao.markkenneth@gmail.com>
2020-04-19 14:47:56 -0700

    Voting Page proof of concept

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,108 +1,128 @@
-import React, { useState, Fragment, useEffect } from 'react';
-import { NavLink, Link, useHistory, useLocation } from 'react-router-dom';
-import '../styles/Result.css';
+import React, { useState, Fragment, useEffect } from "react";
+import { NavLink, Link, useHistory, useLocation } from "react-router-dom";
+import "../styles/Result.css";
 
 const Result = () => {
-	const history = useHistory();
-	const [ question, setQuestion ] = useState('');
-	const [ answers, setAnswers ] = useState([]);
-	const [ errorPage, setErrorPage ] = useState(false);
-	//Handling the set value for percentage
-	const handleInputChange = numberVotes => {
-		var index = 0;
-		var total = 0;
-		for (index = 0; index < answers.length; index++) {
-			total = total + Number(answers[index].answerCount);
-		}
-		return Number((numberVotes / total * 100).toFixed(1));
-	};
-	//Function for getting the unique id
-	const useQuery = () => {
-		return new URLSearchParams(useLocation().search);
-	};
-	//Calling the function
-	let query = useQuery();
-	//Calling the database
-	const getData = async () => {
-		let uniqueID = query.get('uid');
-		const getUser = await fetch(`https://ez-poll.firebaseio.com/qna/${uniqueID}.json`);
-		const resUserData = await getUser.json();
-		if (resUserData == null) setErrorPage(true);
-		else {
-			setQuestion(resUserData.question);
-			setAnswers(resUserData.answers);
-		}
-	};
-	useEffect(() => {
-		getData();
-	}, []);
-	//Handling the share button
-	const sharePoll = async e => {
-		e.preventDefault();
-		history.push('/');
-	};
-	//Handling the new button
-	const newPoll = async e => {
-		e.preventDefault();
-		history.push('/');
-	};
-	return errorPage != true ? (
-		<div className='Result'>
-			<h1 className='textLogo'>EZ Poll</h1>
-			<form className='form'>
-				<div>
-					<input className='inputQuestion' value={question} readOnly />
-					{answers.map((inputField, index) => (
-						<Fragment key={index}>
-							<div className=''>
-								<div className='row'>
-									<div className='column'>
-										<label className='inputAnswerQuestion'>{inputField.answer}</label>
-									</div>
-									<div className='column'>
-										<label className='inputAnswerVotes'>{inputField.answerCount + ' votes'}</label>
-									</div>
-								</div>
+  const history = useHistory();
+  const [question, setQuestion] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [errorPage, setErrorPage] = useState(false);
 
-								<div className='row'>
-									<div className='column'>
-										{index.answerCount === 0 ? (
-											<div
-												className='w3-progress'
-												style={{ width: handleInputChange(inputField.answerCount) + '%' }}
-											/>
-										) : (
-											<div
-												className='w3-progress'
-												style={{ width: handleInputChange(inputField.answerCount) + '%' }}
-											>
-												40%
-											</div>
-										)}
-									</div>
-									<div className='column'>
-										<label className='inputAnswerVotes'>
-											{handleInputChange(inputField.answerCount) + '%'}
-										</label>
-									</div>
-								</div>
-							</div>
-						</Fragment>
-					))}
-				</div>
-			</form>
-			<button type='submit' className='buttonPoll' onClick={sharePoll}>
-				Share Poll
-			</button>
-			<button type='submit' className='buttonPoll' onClick={newPoll}>
-				New Poll
-			</button>
-		</div>
-	) : (
-		<div className='Result'>
-			<h1 className='textLogo'>Error</h1>
-			<h1 className='errorText'> Path does not exist!!</h1>
-		</div>
-	);
+  //Handling the set value for percentage
+  const handleInputChange = (numberVotes) => {
+    var index = 0;
+    var total = 0;
+    for (index = 0; index < answers.length; index++) {
+      total = total + Number(answers[index].answerCount);
+    }
+    return Number(((numberVotes / total) * 100).toFixed(1));
+  };
+
+  //Function for getting the unique id
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  //Calling the function
+  let query = useQuery();
+
+  //Calling the database
+  const getData = async () => {
+    let uniqueID = query.get("uid");
+    const getUser = await fetch(
+      `https://ez-poll.firebaseio.com/qna/${uniqueID}.json`
+    );
+    const resUserData = await getUser.json();
+    if (resUserData == null) setErrorPage(true);
+    else {
+      setQuestion(resUserData.question);
+      setAnswers(resUserData.answers);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //Handling the share button
+  const sharePoll = async (e) => {
+    e.preventDefault();
+    history.push("/");
+  };
+
+  //Handling the new button
+  const newPoll = async (e) => {
+    e.preventDefault();
+    history.push("/");
+  };
+
+  return errorPage != true ? (
+    <div className="Result">
+      <h1 className="textLogo">EZ Poll</h1>
+      <form className="form">
+        <div>
+          <input className="inputQuestion" value={question} readOnly />
+          {answers.map((inputField, index) => (
+            <Fragment key={index}>
+              <div className="">
+                <div className="row">
+                  <div className="column">
+                    <label className="inputAnswerQuestion">
+                      {inputField.answer}
+                    </label>
+                  </div>
+                  <div className="column">
+                    <label className="inputAnswerVotes">
+                      {inputField.answerCount + " votes"}
+                    </label>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="column">
+                    {index.answerCount === 0 ? (
+                      <div
+                        className="w3-progress"
+                        style={{
+                          width:
+                            handleInputChange(inputField.answerCount) + "%",
+                        }}
+                      />
+                    ) : (
+                      <div
+                        className="w3-progress"
+                        style={{
+                          width:
+                            handleInputChange(inputField.answerCount) + "%",
+                        }}
+                      >
+                        40%
+                      </div>
+                    )}
+                  </div>
+                  <div className="column">
+                    <label className="inputAnswerVotes">
+                      {handleInputChange(inputField.answerCount) + "%"}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </Fragment>
+          ))}
+        </div>
+      </form>
+      <button type="submit" className="buttonPoll" onClick={sharePoll}>
+        Share Poll
+      </button>
+      <button type="submit" className="buttonPoll" onClick={newPoll}>
+        New Poll
+      </button>
+    </div>
+  ) : (
+    <div className="Result">
+      <h1 className="textLogo">Error</h1>
+      <h1 className="errorText"> Path does not exist!!</h1>
+    </div>
+  );
 };
 export default Result;
--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,23 +1,129 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-import '../styles/Vote.css';
+import React, { useState, Fragment, useEffect } from "react";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
+import "../styles/Vote.css";
+import { databaseURL } from "../components/Home";
 
 const Vote = () => {
-	return (
-		<div className='Vote'>
-			<p>Vote Page</p>
-			<NavLink to='/results'>
-				<button type='submit' className='button'>
-					Submit
-				</button>
-			</NavLink>
-			<NavLink to='/results'>
-				<button type='submit' className='button'>
-					Results
-				</button>
-			</NavLink>
-		</div>
-	);
+  const history = useHistory();
+  const [question, setQuestion] = useState(null);
+  const [answers, setAnswers] = useState(null);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(0);
+  const [uniqueID, setUniqueID] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  let query = new URLSearchParams(useLocation().search);
+
+  const getData = async () => {
+    //Check browser link first
+    let uniqueID = query.get("uid");
+    if (uniqueID) {
+      setUniqueID(uniqueID);
+    } else {
+      return; //TODO: Go to error page
+    }
+
+    //Fetch from Firebase
+    const getUser = await fetch(
+      `https://ez-poll.firebaseio.com/qna/${uniqueID}.json`
+    );
+    const resUserData = await getUser.json();
+    if (resUserData) {
+      setQuestion(resUserData.question);
+      setAnswers(resUserData.answers);
+    }
+  };
+
+  const onSubmitButton = async (e) => {
+    e.preventDefault();
+    //Grab most recent vote count snapshot
+    const mostRecentAnswers = await fetch(
+      `https://ez-poll.firebaseio.com/qna/${uniqueID}.json`
+    );
+    const recentAnswerJSON = await mostRecentAnswers.json();
+    if (recentAnswerJSON == null) {
+      console.log("No answers!");
+      return;
+    }
+    //Increment vote count locally
+    const newAnswers = recentAnswerJSON.answers;
+    const oldAnswer = newAnswers[selectedAnswerIndex];
+    newAnswers[selectedAnswerIndex] = {
+      ...oldAnswer,
+      answerCount: oldAnswer.answerCount + 1,
+    };
+    //Update value in Firebase Database
+    const response = await fetch(
+      `https://ez-poll.firebaseio.com/qna/${uniqueID}.json`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          question,
+          answers: newAnswers,
+        }),
+      }
+    );
+    const resData = await response.json();
+  };
+
+  const onShareButton = async (e) => {
+    e.preventDefault();
+    history.push("/");
+  };
+
+  const onNewPollButton = async (e) => {
+    e.preventDefault();
+    history.push("/");
+  };
+
+  const actualAnswers = () => {
+    return (
+      <div className="votePageBox">
+        <div className="votePageQuestionText">{question}</div>
+        <div className="votePageAnswersContainer">
+          {answers.map((answer, index) => (
+            <Fragment key={index}>
+              <div className="radio">
+                <label className="votePageAnswersText">
+                  <input
+                    type="radio"
+                    value={answer.answer}
+                    checked={index === selectedAnswerIndex}
+                    className="radioCircle"
+                    onChange={() => setSelectedAnswerIndex(index)}
+                  />
+                  {answer.answer}
+                </label>
+              </div>
+            </Fragment>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="Vote">
+      <h1 className="votePageLogo">EZ Poll</h1>
+      <div className="votePageBoxContainer">
+        {answers && question && actualAnswers()}
+      </div>
+      <button type="submit" className="buttonPoll" onClick={onSubmitButton}>
+        Submit
+      </button>
+      <button type="submit" className="buttonPoll" onClick={onShareButton}>
+        Share
+      </button>
+      <button type="submit" className="buttonPoll" onClick={onNewPollButton}>
+        New Poll
+      </button>
+    </div>
+  );
 };
+
 export default Vote;
